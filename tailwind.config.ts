0ee70548e28ddd7cbfd30bfa69dfec07727508f4
Utsav Dey <6429119+coDey1410@users.noreply.github.com>
2024-07-08 17:41:23 +0530

    n3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {
